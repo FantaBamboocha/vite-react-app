81b0000d680abd25995f7664ed5b4db8a7576edf
FantaBamboocha <moomentomoori@gmail.com>
2024-01-24 18:01:42 +0300

    optimization of requests to the server using lodash.debounce

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,17 +1,31 @@
+import { useCallback, useRef, useState } from "react";
 import { useDispatch } from "react-redux";
+import debounce from "lodash.debounce";
 
 import { setSearchValue } from "../../redux/slices/filter";
 
 import styles from "./styles.module.scss";
 
 const Search = ({ searchValue }) => {
+  const [localSearchValue, setLocalSearchValue] = useState("");
+  const inputRef = useRef();
   const dispatch = useDispatch();
   const handleClear = () => {
     dispatch(setSearchValue(""));
+    setLocalSearchValue("");
+    inputRef.current.focus();
   };
 
+  const handleDebouncedChange = useCallback(
+    debounce((e) => {
+      dispatch(setSearchValue(e.target.value));
+      console.log(e.target.value);
+    }, 1000),
+    []
+  );
   const handleChange = (e) => {
-    dispatch(setSearchValue(e.target.value));
+    setLocalSearchValue(e.target.value);
+    handleDebouncedChange(e);
   };
 
   return (
@@ -26,12 +40,13 @@ const Search = ({ searchValue }) => {
         <path d="M368 208A160 160 0 1 0 48 208a160 160 0 1 0 320 0zM337.1 371.1C301.7 399.2 256.8 416 208 416C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208c0 48.8-16.8 93.7-44.9 129.1L505 471c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0L337.1 371.1z" />
       </svg>
       <input
-        value={searchValue}
+        ref={inputRef}
+        value={localSearchValue}
         onChange={handleChange}
         type="text"
         placeholder="Поиск..."
       />
-      {searchValue && (
+      {localSearchValue && (
         <svg
           className={styles.clearIcon}
           onClick={handleClear}
